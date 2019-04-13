959f244695eca260653d521870cc873766d9fc5c
Stefan Gwihs <stefan.gwihs@nagarro.com>
2019-04-13 23:59:40 +0200

    added eslint and automatically lint on change

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,6 +4,8 @@ import path from 'path';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+/* eslint-disable no-console */
+
 const port = 3000;
 const app = express();
 
